d79909e4d0d20aa2af8030206adf54ef5ef9b86a
Patrick Kohan <patrick.kohan@googlemail.com>
2013-04-06 23:44:02 +0200

    blatt3 import

---
+++ src/blatt3/BullyProcess.java
@@ -0,0 +1,21 @@
+package blatt3;
+
+/**
+ * User: mogli
+ * Date: 06.04.13
+ * Time: 23:40
+ */
+public class BullyProcess extends Process {
+    public BullyProcess(int i) {
+        super(i);
+    }
+
+    @Override
+    public void startElection() {
+    }
+
+
+    @Override
+    public void run() {
+    }
+}
---
+++ src/blatt3/GlobalSystem.java
@@ -0,0 +1,28 @@
+package blatt3;
+
+public class GlobalSystem {
+	private final static int PROCESSES = 10;
+
+	void run() throws InterruptedException {
+		Process[] processes = new Process[PROCESSES];
+
+		for (int i = 0; i < processes.length; i++)
+			processes[i] = new BullyProcess(i);
+
+		for (Process source : processes) {
+			for (Process destination : processes)
+				if (!source.equals(destination))
+					source.connect(destination);
+			new Thread(source).start();
+		}
+
+		processes[8].setActive(false);
+		processes[9].setActive(false);
+		processes[0].startElection();
+	}
+
+	public static void main(String[] args) throws InterruptedException {
+		new GlobalSystem().run();
+	}
+
+}
